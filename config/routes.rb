a72d8d088b9bf81ba4462dfe926d9e5c25d3b5b8
Augusto della Faille <augusto.dellafaille@gmail.com>
2015-10-06 14:44:14 +0200

    filter work

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get '/not_available' => "welcome#not_available" ,as: 'not_available'
+  get '/sorry' => "welcome#sorry", as: 'sorry'
+  get '/available'=> "welcome#available", as: 'available'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
