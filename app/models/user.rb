57303388737e96cac1c93d1306a444b3e59881c1
Augusto della Faille <augusto.dellafaille@gmail.com>
2015-10-10 13:11:45 +0200

    validation are put in place but not activate

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ActiveRecord::Base
   validates :firstname, presence: false
   validates :lastname, presence: false
   validates :date_of_birth, presence: false
-  # validates_date :birthday, :before => lambda { 16.years.ago },
-  #                              :before_message => "Vous devez être âgé de plus de 16 ans"
-  # validates_date :birthday, :on_or_after => lambda { 125.years.ago },
-  #                              :before_message => "Êtes-vous certain d'être né au 19ème siècle ?"
-  # validates :sexe, presence: false, inclusion: {in: ["Male","Female"]}
+  validates_date :date_of_birth, :before => lambda { 16.years.ago },
+                               :before_message => "Vous devez avoir 16 ans"
+  validates_date :date_of_birth, :on_or_after => lambda { 125.years.ago },
+                                :before_message => "Êtes-vous certain d'être né au 19ème siècle ?"
+  validates :sexe, presence: false, inclusion: {in: ["Male","Female"]}
   validates :address, presence: false
   validates :zip_code, presence: false
   validates :city, presence: false
-  # validates :phone_number, :phony_plausible => true, presence: false
-  # validates :status, :inclusion => {:in => [true, false]}, presence: false
+  validates :phone_number, :phony_plausible => true, presence: false
+  validates :status, :inclusion => {:in => [true, false]}, presence: false
   validates :avg_rating_user, presence: false
   geocoded_by :full_street_address
 
