94b5f76248824b75e8d6a37bf00ed392b9a81b1f
doei <doei@users.noreply.github.com>
2015-10-12 23:50:04 +0200

    first functional profile

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -13,8 +13,39 @@ $('a').click(function(){
     return false;
 });
 
+//Navbar scroll detection : -------------------------------------------------------
+var docElem = document.documentElement,
+		header = document.querySelector( '.navbar-default' ),
+		didScroll = false,
+		changeHeaderOn = $(window).height() - 150;
 
-// Hide SVG paths :
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 250 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'navbar-shrink' );
+		}
+		else {
+			classie.remove( header, 'navbar-shrink' );
+		}
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+	init();
+
+// Hide SVG paths :-------------------------------------------------------
 var pathlist = document.querySelectorAll("path");
 
 var hideAllPaths = function() {
