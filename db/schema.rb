f2a55bbeae36f89f402c1c38d8e874e86608fece
Julian Philip <julian.philip@essec.edu>
2015-10-13 17:59:08 +0200

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012094242) do
+ActiveRecord::Schema.define(version: 20151013101151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151012094242) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "quantity"
+    t.text     "comment"
   end
 
   add_index "ordered_products", ["order_id"], name: "index_ordered_products_on_order_id", using: :btree
