94473b73c69bf8e8c9bdd9d99ffdb3c41e9bac62
Julian Philip <julian.philip@essec.edu>
2015-10-15 18:35:14 +0200

    quantity fixed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    UserMailer.list_user(@order).deliver
+    # UserMailer.list_user(@order).deliver
     send_sms_list(@order.shopper, @order.user)
   end
 
